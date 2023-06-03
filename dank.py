f732a80a10b640d5cfcd2574074f1e95c710e78c
Ex-Opera <88405792+Ex-Opera@users.noreply.github.com>
2023-06-04 01:53:04 +0530

    Update dank.py
    Made it cleaner

--- dank.py
+++ dank.py
@@ -1,17 +1,18 @@
 import requests
 import time
+
 channelID = int(input("Input channel_ID: "))
 headers = {"authorization": "Token here"}
-def dank():
-  requests.post(f'https://discord.com/api/v9/channels/{channelID}/messages', data = payload, headers = headers)
-  if payload == {'content': 'pls fish'}: print(f'fish {i+1}')
-  elif payload == {'content': 'pls hunt'}: print(f'hunt {i+1}')
-  elif payload == {'content': 'pls dig'}: print(f'dig {i+1}')
-  elif payload == {'content': 'pls beg'}: print(f'beg {i+1}')
+
+def dank(action):
+  payload = {'content': f'pls {action}'}
+  requests.post(f'https://discord.com/api/v9/channels/{channelID}/messages', data=payload, headers=headers)
+  print(f'{action} {i+1}')
+
 repeat = int(input("Input number of repeat: "))
+actions = ['fish', 'hunt', 'dig', 'beg']
+
 for i in range(repeat):
-  payload = {'content': 'pls fish'}; dank()
-  payload = {'content': 'pls hunt'}; dank()
-  payload = {'content': 'pls dig'}; dank()
-  payload = {'content': 'pls beg'}; dank()
+  for action in actions:
+    dank(action)
   time.sleep(35)
