12fa40e24f8e26e00a118739a7999b6081ddf90e
Ex-Opera <88405792+Ex-Opera@users.noreply.github.com>
2023-11-12 13:14:34 +0530

    Update dank_automation.py

--- dank_automation.py
+++ dank_automation.py
@@ -2,16 +2,16 @@ import requests
 import time
 from dank_analysis import display_time_info
 
-def perform_dank_action(action):
+def perform_dank_action(action, i):
     payload = {"content": f"pls {action}"}
     requests.post(f"https://discord.com/api/v9/channels/{channel_id}/messages", data=payload, headers={"authorization": discord_token})
-    print(f"{action} executed {i + 1} times")
+    return i+1
 
 def automate_coin_collection(repeat_count, action_list):
     for i in range(repeat_count):
         for action in action_list:
-            perform_dank_action(action)
-        completed_executions += len(action_list)
+            completed_executions = perform_dank_action(action, i)
+        print("Set of dank actions performed: {completed_executions}")
         time.sleep(35)
 
 # Replace "Token here" with your actual Discord bot token
@@ -20,7 +20,6 @@ channel_id = int(input("Input Discord channel ID: "))
 
 # Call the function to display estimated time
 total_executions = int(input("Input the total number of planned executions: "))
-completed_executions = 0
 actions_list = ['fish', 'hunt', 'dig', 'beg']
 
 display_time_info(total_executions, completed_executions)
